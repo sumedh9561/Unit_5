da377039425d764dc3b2930b7781759e8c1c4e33
Sumedh Ambhore <103955283+sumedh9561@users.noreply.github.com>
2022-10-16 15:36:54 +0530

    Add files via upload
    add

---
+++ Day_1/src/Problem_1/Main.java
@@ -0,0 +1,27 @@
+package Problem_1;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		
+		String Name="sumedh";
+		String Fathers_Name="Rajkumar";
+		String Mother_Name="Mina";
+		int Age =23;
+		String Gender="Male";
+		String Addres="At-post-sasti,Ta-Patur,Dist-Akoa";
+		double Mobile_No=9561208956d;
+		
+		System.out.println("My name is " + Name);
+		System.out.println(Fathers_Name);
+		System.out.println(Mother_Name);
+		System.out.println(Age);
+		System.out.println(Gender);
+		System.out.println(Addres);
+		System.out.println(Mobile_No);
+		
+		
+	}
+	
+
+}
